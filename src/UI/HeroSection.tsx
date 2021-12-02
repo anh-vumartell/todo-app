34c46a597f4fee327ed53df81d99a05bc533eb2d
Ngoc Anh Vu-Martell <anh.vumartell@github.io>
2021-12-02 09:55:00 +0200

    update UI

---
+++ src/UI/HeroSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HeroSection() {
+  return <div className="hero-section"></div>;
+}
+
+export default HeroSection;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -6,9 +6,23 @@ const Todos: React.FC = () => {
   const { removeTodo, items } = useTodoCtx();
   return (
     <ul className="todos">
+      {items.length === 0 && (
+        <p className="alert-text">
+          Oops! Todo list is empty. Maybe adding one?
+        </p>
+      )}
       {items.map((item) => (
         <TodoItem key={item.id} onRemoveTodo={removeTodo} item={item} />
       ))}
+      <div className="todo-tracker">
+        <span>
+          {items.length > 1 ? `${items.length} items` : `${items.length} item`}{" "}
+          left
+        </span>
+        <span>All</span>
+        <span>Active</span>
+        <span>Completed</span>
+      </div>
     </ul>
   );
 };
