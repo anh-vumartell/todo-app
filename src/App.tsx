2a299f362f8d1c4f6c25f569f479cb53a04552cd
Ngoc Anh Vu-Martell <anh.vumartell@github.io>
2021-12-08 14:48:47 +0200

    update page footer

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,21 @@ function App() {
           <NewTodo />
           <Todos />
         </main>
+        <footer>
+          <div className="footer-text">
+            <p>Minimal Todo App © 2021</p>
+            <p>
+              Designed & coded by{" "}
+              <a href="https://github.com/anh-vumartell/todo-app">
+                Anh Vu-Martell
+              </a>
+            </p>
+            <p>
+              Image credit to contributor from{" "}
+              <a href="https://unsplash.com/photos/0jIovxJj7pY">Unplash</a>
+            </p>
+          </div>
+        </footer>
       </div>
     </TodosContextProvider>
   );
